8c9d5e8c76da38c076d120f770249b8aa3bab9f4
cyyoun <cyyoun41@gmail.com>
2023-09-05 22:32:23 +0900

    환경세팅 - 라이브러리 및 DB 연결(MySQL)

---
+++ src/test/java/cyng/mmview/MmviewApplicationTests.java
@@ -0,0 +1,13 @@
+package cyng.mmview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MmviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
